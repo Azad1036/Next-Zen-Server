724c2f1eee756fa119811516276d35576bb522c6
Azad1036 <mohammadazadhossain2024@gmail.com>
2025-03-12 22:51:56 +0600

    vercel setup and deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 4000;
@@ -8,7 +9,8 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@firstproject.mz7uu.mongodb.net/?retryWrites=true&w=majority&appName=FirstProject`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,13 +29,6 @@ async function mainDB() {
   const compaignCollection = client.db("compaignDB").collection("compaign");
   const donatedCollection = client.db("donatedDB").collection("donation");
   try {
-    // await client.connect();
-    // // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
-
     // Display Campaign
     app.get("/campaigns", async (req, res) => {
       const allCampaignData = compaignCollection.find();
@@ -43,7 +38,7 @@ async function mainDB() {
 
     app.get("/runningCampaigns", async (req, res) => {
       const limit = parseInt(req.query.limit) || 0;
-      const allCampaignData = compaignCollection.find();
+      const allCampaignData = compaignCollection.find().limit(6);
       const result = await allCampaignData.toArray();
       res.send(result);
     });
@@ -68,8 +63,6 @@ async function mainDB() {
     app.put("/updateCampaign/:id", async (req, res) => {
       const id = req.params.id;
       const quary = { _id: new ObjectId(id) };
-      // const update = await compaignCollection.findOne(quary);
-      // res.send(update)
       const user = req.body;
       const option = { upsert: true };
       const updateCampaign = {
@@ -98,8 +91,9 @@ async function mainDB() {
       res.send(result);
     });
 
-    // Donation Releted api
 
+
+    // Donation Releted api
     // my donation
     app.get("/myDonation/:email", async (req, res) => {
       const email = req.params.email;
@@ -115,8 +109,7 @@ async function mainDB() {
       res.send(result);
     });
   } catch (error) {
-    console.log(error);
-    await client.close();
+    // await client.close();
   }
 }
 
